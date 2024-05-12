067e1c714b87d791d2fa6cf31280d3eb08655a3c
Gleidson G. de Oliveira dos Santos <gleidsonguilhermecpd@hotmail.com>
2024-05-11 23:51:48 -0300

    Key vault crud fixes

--- src/Microsoft.Azure.Library/Microsoft.Azure.Library.Worker/Program.cs
+++ src/Microsoft.Azure.Library/Microsoft.Azure.Library.Worker/Program.cs
@@ -6,26 +6,39 @@ class Program
     static async Task Main(string[] args)
     {
         string keyVaultName = "kv-library";
-        string secretName = "kv-connection-string";
-
         var keyVaultCrud = new KeyVaultService(keyVaultName);
 
-        // Criar ou atualizar um segredo
-        //await keyVaultCrud.CreateOrUpdateSecretAsync(secretName, "secret-value");
+        #region Criar ou atualizar um segredo
+        string secretNameCreateOrUpdate = "kv-connection-string-1";
+        string secretValueCreateOrUpdate = "Server=localhost;Port=9696;Database=teste;Uid=teste;Pwd=123456789";
+        await keyVaultCrud.CreateOrUpdateSecretAsync(secretNameCreateOrUpdate, secretValueCreateOrUpdate);
+        Console.WriteLine("Segredo criado com sucesso");
+        #endregion
 
-        // Obter um segredo
-        SecretModel secret = await keyVaultCrud.GetSecretAsync(secretName);
-        Console.WriteLine($"Valor do segredo '{secret.Name}': {secret.Value}");
+        #region Obter um segredo
+        string secretNameGet = "kv-connection-string-1";
+        SecretModel secretGet = await keyVaultCrud.GetSecretAsync(secretNameGet);
+        Console.WriteLine($"Valor do segredo criado no passo anterior '{secretGet.Name}': {secretGet.Value}");
+        #endregion
 
-        // Atualizar um segredo
-        //await keyVaultCrud.UpdateSecretAsync(secretName, "new-secret-value");
+        #region Atualizar um segredo
+        string secretNameUpdate = "kv-connection-string-1";
+        string secretValueUpdate = "Server=localhost;Port=9797;Database=teste;Uid=teste;Pwd=123456789";
+        await keyVaultCrud.UpdateSecretAsync(secretNameUpdate, secretValueUpdate);
+        Console.WriteLine("Segredo atualizado com sucesso!!!");
+        #endregion
 
-        // Obter o segredo atualizado
-        //secret = await keyVaultCrud.GetSecretAsync(secretName);
-        //Console.WriteLine($"Valor do segredo '{secret.Name}': {secret.Value}");
+        #region Obter o segredo atualizado
+        string secretNameGetSecretUpdate = "kv-connection-string-1";
+        SecretModel secretGetSecretUpdate = await keyVaultCrud.GetSecretAsync(secretNameGetSecretUpdate);
+        Console.WriteLine($"Valor do segredo '{secretGetSecretUpdate.Name}': {secretGetSecretUpdate.Value}");
+        #endregion
 
-        //// Excluir um segredo
-        //await keyVaultCrud.DeleteSecretAsync(secretName);
+        #region Excluir um segredo
+        string secretNameDeleteSecret = "kv-connection-string-1";
+        await keyVaultCrud.DeleteSecretAsync(secretNameDeleteSecret);
+        Console.WriteLine("Segredo deletado com sucesso!!!");
+        #endregion
 
         Console.ReadKey();
     }
